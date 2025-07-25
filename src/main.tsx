a0627a4ad07ad2d761ec0b5e3e6a3048a5b76bde
AlexLeonov2020 <alex.leon2010@icloud.com>
2025-07-25 21:03:14 +0300

    first

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './components/App/App.tsx';
-import 'modern-normalize/modern-normalize.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './components/App/App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import "modern-normalize"
+
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </StrictMode>
-);
+      <ReactQueryDevtools initialIsOpen={ false } />
+      </QueryClientProvider>
+  </StrictMode>,
+)
