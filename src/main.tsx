64068187a811244a2bdbe367f0dc1fca4538b8a3
AlexLeonov2020 <alex.leon2010@icloud.com>
2025-07-26 09:01:15 +0300

    set

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './components/App/App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import "modern-normalize"
+import 'modern-normalize/modern-normalize.css'
 
 
 const queryClient = new QueryClient();
